68730958555e0680b01dbd094601841903b0af67
VictorMPstudent <132060712+VictorMPstudent@users.noreply.github.com>
2023-06-10 14:17:10 +0200

    added event listeners to the drums

---
+++ src/pages/Drums/index.js
@@ -0,0 +1,6 @@
+for(i=0;i<document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        //sonido  
+        //boton animación             
+    });
+}
