ce5b9205e5498c5fc1ffdcdb390df027c1a95999
DanielGispert <daniel.gispert@estudiantat.upc.edu>
2023-06-22 16:42:42 +0200

    restructuracion JS + images + crate Tinder Dog + try if it's working

---
+++ public/indes.js
@@ -0,0 +1,43 @@
+function ocultar1(){
+    document.getElementById('profile-container').style.display = 'none';
+}
+function ocultar2(){
+    document.getElementById('profile-container2').style.display = 'none';
+}
+function ocultar3(){
+    document.getElementById('profile-container3').style.display = 'none';
+}
+function mostrar1(){
+    document.getElementById('profile-container-me-gusta').style.display = 'block';
+    document.getElementById('profile-container').style.display = 'none';
+}
+function mostrar2(){
+    document.getElementById('profile-container2-me-gusta').style.display = 'block';
+    document.getElementById('profile-container2').style.display = 'none';
+}
+function mostrar3(){
+    document.getElementById('profile-container3-me-gusta').style.display = 'block';
+    document.getElementById('profile-container3').style.display = 'none';
+}
+function tornar(){
+    document.getElementById('profile-container-me-gusta').style.display = 'none';
+    document.getElementById('profile-container').style.display = 'block';
+}
+function tornar2(){
+    document.getElementById('profile-container2-me-gusta').style.display = 'none';
+    document.getElementById('profile-container2').style.display = 'block';
+}
+function tornar3(){
+    document.getElementById('profile-container3-me-gusta').style.display = 'none';
+    document.getElementById('profile-container3').style.display = 'block';
+}
+function contacte1(){
+    document.getElementById('Contacte').style.display = 'block';
+}
+function contacte2(){
+    document.getElementById('Contacte2').style.display = 'block';
+}
+function contacte3(){
+    document.getElementById('Contacte3').style.display = 'block';
+ }
+ 
